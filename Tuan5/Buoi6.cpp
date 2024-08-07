6450f297a0f689fd296c8ccbfb56d51829dba79e
Ho Thien <hothien700@gmail.com>
2024-08-07 13:56:21 +0700

    Add file Buoi6_bai1.cpp and Kq_chuoi and debug1

--- Tuan5/Buoi6.cpp
+++ Tuan5/Buoi6.cpp
@@ -1,18 +1,18 @@
-#include <stdio.h>
-#include <conio.h>
-#include <stdlib.h>
-void Thap(int n, char x, char y, char z){
-	if (n > 0){
-		Thap(n - 1, x, z, y);
-		printf("\n%c-> %c",x,y);
-		Thap(n - 1, z, y, x);
-	}
-}
-
-int main(){
-	int n;
-	printf("\nNhap so dia cua thap:");
-	scanf_s("%d ", &n);
-	Thap(n, 'A', 'B', 'C');
-	_getch();
-}
+//#include <stdio.h>
+//#include <conio.h>
+//#include <stdlib.h>
+//void Thap(int n, char x, char y, char z){
+//	if (n > 0){
+//		Thap(n - 1, x, z, y);
+//		printf("\n%c-> %c",x,y);
+//		Thap(n - 1, z, y, x);
+//	}
+//}
+//
+//int main(){
+//	int n;
+//	printf("\nNhap so dia cua thap:");
+//	scanf_s("%d ", &n);
+//	Thap(n, 'A', 'B', 'C');
+//	_getch();
+//}
